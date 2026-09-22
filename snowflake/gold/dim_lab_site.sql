c3160bfdc642c2c7ca73b983e1cc3b70d1898940
6siddharthp <siddharth.poddar@gmail.com>
2026-09-22 08:37:55 -0500

    Lock Gold Layer: 8 objects

--- snowflake/gold/dim_lab_site.sql
+++ snowflake/gold/dim_lab_site.sql
@@ -1,3 +1,3 @@
 -- Foundation Studio · Snowflake execution SQL
 -- foundation:stage 2
-CREATE OR REPLACE TABLE OGFS_DEMO.GOLD.dim_lab_site AS SELECT column1::VARCHAR site_key,column2::VARCHAR site_code,column3::VARCHAR site_name FROM VALUES ('SITE_BUENOS_AIRES','buenos-aires','Buenos Aires'),('SITE_ANNANDALE','annandale','Annandale'),('SITE_CURITIBA','curitiba','Curitiba'),('SITE_HOUSTON','houston','Houston');
+CREATE OR REPLACE TABLE OGFS_DEMO.GOLD.dim_lab_site AS SELECT column1::VARCHAR site_key,column2::VARCHAR site_code,column3::VARCHAR site_name FROM VALUES ('SITE_ANNANDALE','annandale','Annandale'),('SITE_HOUSTON','houston','Houston'),('SITE_CURITIBA','curitiba','Curitiba'),('SITE_BUENOS_AIRES','buenos_aires','Buenos Aires');
