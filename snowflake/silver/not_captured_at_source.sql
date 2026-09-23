c03e3f687c0febaedbde57c151c61510b10bed01
6siddharthp <siddharth.poddar@gmail.com>
2026-09-23 11:31:19 -0500

    Push generated Silver SQL: 23 files

--- snowflake/silver/not_captured_at_source.sql
+++ snowflake/silver/not_captured_at_source.sql
@@ -3,8 +3,7 @@
 CREATE OR REPLACE TABLE OGFS_DEMO.SILVER.not_captured_at_source AS
 SELECT column1::VARCHAR source_table,column2::VARCHAR site_code,column3::VARCHAR site_name,
        column4::VARCHAR canonical_field,column5::VARCHAR reason
-FROM VALUES ('LIMS_CURITIBA_AMOSTRAS','curitiba','Curitiba','analyst_id','Not captured at source'),
-('LIMS_CURITIBA_AMOSTRAS','curitiba','Curitiba','project_reference','Not captured at source'),
+FROM VALUES ('LIMS_CURITIBA_AMOSTRAS','curitiba','Curitiba','project_reference','Not captured at source'),
 ('LIMS_CURITIBA_AMOSTRAS','curitiba','Curitiba','spec_lower_limit','Not captured at source'),
 ('LIMS_CURITIBA_AMOSTRAS','curitiba','Curitiba','spec_upper_limit','Not captured at source'),
 ('LIMS_CURITIBA_AMOSTRAS','curitiba','Curitiba','approval_date','Not captured at source'),
