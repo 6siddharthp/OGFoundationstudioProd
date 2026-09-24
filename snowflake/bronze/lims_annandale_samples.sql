e76aef69b74859e047b14b915ccc2637b1b0da1d
6siddharthp <siddharth.poddar@gmail.com>
2026-09-24 16:18:24 -0500

    Deploy Snowflake package: 47 SQL files

--- snowflake/bronze/lims_annandale_samples.sql
+++ snowflake/bronze/lims_annandale_samples.sql
@@ -1,6 +1,7 @@
 -- Foundation Studio · Snowflake execution SQL
 -- updated on Wednesday by Sid V3 . tuesday change was somne with Ankit
 -- Chetan ji
+
 -- foundation:stage 2
 CREATE OR REPLACE TABLE OGFS_DEMO.BRONZE.lims_annandale_samples AS
 SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS source_row_number,
